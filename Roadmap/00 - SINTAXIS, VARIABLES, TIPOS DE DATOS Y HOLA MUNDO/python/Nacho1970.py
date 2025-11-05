cd9a6dff0115acc856f958271589e09b6f8ea3f4
Nacho DT <jidapet@gmail.com>
2025-11-05 09:05:25 +0100

    commit

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Nacho1970.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Nacho1970.py
@@ -1,5 +1,7 @@
 # http://python.org
 
+# Comentario en una línea
+
 """
 Comentario en
 varias líneas
@@ -15,9 +17,16 @@ mi_variable = "esta es una variable"
 MI_CONSTANTE = 1970 # En Python no existen las constantes, pero se pueden hacer variables, indicando por medio de mayúsculas que es una constante, aunque no lo es.
 
 cadena_texto = "Cadena de texto o str"
+cadena_texto_2 = 'Cadena de texto mediante comillas simples o str'
 cadena_entero = 123 # Es un int
 cadena_coma_flotante = 1.23 # Es un número decimal
 cadena_booleano = False # Es un boolean
+
+print(type(cadena_texto))
+print(type(cadena_texto_2))
+print(type(cadena_entero))
+print(type(cadena_coma_flotante))
+print(type(cadena_booleano))
 
 lenguaje= "Python"
 print(f"Hola, {lenguaje}!!")
