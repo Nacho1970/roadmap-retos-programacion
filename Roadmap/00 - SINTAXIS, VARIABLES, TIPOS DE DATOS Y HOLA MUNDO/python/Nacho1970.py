054490463e5fedcf5014360284f0c236e879b300
Nacho DT <jidapet@gmail.com>
2025-11-05 09:00:28 +0100

    commit

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Nacho1970.py
@@ -0,0 +1,23 @@
+# http://python.org
+
+"""
+Comentario en
+varias líneas
+"""
+
+'''
+Otro comentario en
+varias líneas
+'''
+
+mi_variable = "esta es una variable"
+
+MI_CONSTANTE = 1970 # En Python no existen las constantes, pero se pueden hacer variables, indicando por medio de mayúsculas que es una constante, aunque no lo es.
+
+cadena_texto = "Cadena de texto o str"
+cadena_entero = 123 # Es un int
+cadena_coma_flotante = 1.23 # Es un número decimal
+cadena_booleano = False # Es un boolean
+
+lenguaje= "Python"
+print(f"Hola, {lenguaje}!!")
